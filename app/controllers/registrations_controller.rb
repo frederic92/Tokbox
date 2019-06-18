fb665ca61f4a75e5c8ba322bb25ceae35fd23037
frederic <fredericmbea@gmail.com>
2019-06-17 21:02:40 -0400

    Added github_link field to signup

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,6 +7,6 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :email, :avatar, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :github_link, :avatar, :password)
   end
 end
